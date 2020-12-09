0691f77216191d832f29f9c466814cb81ea588a4
Yevhen Shevchenko <sheva9620@gmail.com>
2020-12-09 21:08:40 +0200

    Initial Commit

---
+++ SwiftUI_FirstHW/SwiftUI_FirstHWApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FirstHWApp.swift
+//  SwiftUI_FirstHW
+//
+//  Created by Yevhen Shevchenko on 09.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FirstHWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
